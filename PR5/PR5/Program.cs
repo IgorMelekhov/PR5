34a21d435cd05b5791f036f640266d99f33d7cf8
IgorMelekhov <143938424+IgorMelekhov@users.noreply.github.com>
2023-09-28 19:45:31 +0300

    Add files via upload

---
+++ PR5/PR5/Program.cs
@@ -0,0 +1,67 @@
+﻿//Практическая работа номер 2
+// Выполнил: Мелехов И.В
+// Задание: Выяснить, пройдет ли кирпич в отверстие
+using System;
+
+namespace PR5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int длина, ширина, высота, нижняя_сторона_отверстия, боковая_сторона_отверстия;
+            Console.WriteLine("Практическая работа номер 5");
+            Console.Write("Введите пять значений: \n");
+            Console.Write("длина=");
+            длина = Convert.ToInt32(Console.ReadLine());
+            Console.Write("ширина=");
+            ширина = Convert.ToInt32(Console.ReadLine());
+            Console.Write("высота=");
+            высота = Convert.ToInt32(Console.ReadLine());
+            Console.Write("нижняя_сторона_отверстия=");
+            нижняя_сторона_отверстия = Convert.ToInt32(Console.ReadLine());
+            Console.Write("боковая_сторона_отверстия=");
+            боковая_сторона_отверстия = Convert.ToInt32(Console.ReadLine());
+
+            if (длина <= нижняя_сторона_отверстия)
+            {
+                if (высота <= боковая_сторона_отверстия)
+                    Console.WriteLine("Кирпич пройдет");
+                else Console.WriteLine("Кирпич не пройдет");
+            }
+            if (длина <= боковая_сторона_отверстия)
+            {
+                if (высота <= нижняя_сторона_отверстия)
+                    Console.WriteLine("Кирпич пройдет ");
+                else Console.WriteLine("Кирпич не пройдет");
+            }
+            if (длина <= боковая_сторона_отверстия)
+            {
+                if (ширина <= нижняя_сторона_отверстия)
+                    Console.WriteLine("Кирпич пройдет ");
+                else Console.WriteLine("Кирпич не пройдет");
+            }
+            if (длина <= нижняя_сторона_отверстия)
+            {
+                if (ширина <= боковая_сторона_отверстия)
+                    Console.WriteLine("Кирпич пройдет ");
+                else Console.WriteLine("Кирпич не пройдет");
+            }
+            if (ширина <= боковая_сторона_отверстия)
+            {
+                if (высота <= нижняя_сторона_отверстия)
+                    Console.WriteLine("Кирпич пройдет ");
+                else Console.WriteLine("Кирпич не пройдет");
+            }
+            if (высота <= боковая_сторона_отверстия)
+            {
+                if (ширина <= нижняя_сторона_отверстия)
+                    Console.WriteLine("Кирпич пройдет ");
+                else Console.WriteLine("Кирпич не пройдет");
+            }
+            else Console.WriteLine("Кирпич не пройдет");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ PR5/Program.cs
@@ -0,0 +1,57 @@
+﻿//Практическая работа номер 2
+// Выполнил: Мелехов И.В
+// Задание: Выяснить, пройдет ли кирпич в отверстие
+using System;
+
+namespace temp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool i = false; 
+            Console.WriteLine("Зравствуйте,введите пожалуйста пять переменных");
+
+            int lenght, width, height, LowerSide, LateralSide;
+            Console.Write("длина =");
+            lenght = Convert.ToInt32(Console.ReadLine());
+            Console.Write("ширина=");
+            width = Convert.ToInt32(Console.ReadLine());
+            Console.Write("высота=");
+            height = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Нижняя Сторона = ");
+            LowerSide = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Боковая Сторона = ");
+            LateralSide = Convert.ToInt32(Console.ReadLine());
+            if ((lenght <= LowerSide) && (height <= LateralSide)) 
+                i = true; 
+
+            if ((lenght <= LateralSide) && (height <= LowerSide))
+            {
+                    i = true;
+            }
+            if ((lenght <= LateralSide)&& (width <= LowerSide))
+            {
+                    i = true;
+            }
+            if ((lenght <= LowerSide)&& (width <= LateralSide))
+            {
+                    i = true;
+            }
+            if ((width <= LateralSide)&& (height <= LowerSide))
+            {
+                    i = true;
+            }
+            if ((height <= LateralSide)&& (width <= LowerSide))
+            {
+                    i = true;
+            }
+
+            if (i) Console.WriteLine("Кирпич пройдет");
+            else Console.WriteLine("Кирпич не пройдет");
+
+            Console.ReadKey();
+
+        }
+    }
+}
